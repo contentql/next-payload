b7bcc07804f2830ad55291abec300cb6a9b6c08e
Akhil Naidu <kaparapu.akhilnaidu@gmail.com>
2024-02-07 20:38:23 +0530

    init: generating dashboard using plop

--- client/plopfile.mjs
+++ client/plopfile.mjs
@@ -11,8 +11,29 @@ const plopConfiguration = (/** @type {import('plop').NodePlopAPI} */ plop) => {
     actions: [
       {
         type: 'add',
+        path: 'app/_views/{{pascalCase name}}View.tsx',
+        templateFile: 'templates/dashboard/view.template.hbs',
+      },
+      {
+        type: 'add',
+        path: 'app/(dashboard)/dashboard/{{camelCase name}}/_components/{{pascalCase name}}.tsx',
+        templateFile: 'templates/dashboard/component.template.hbs',
+      },
+      {
+        type: 'add',
         path: 'app/(dashboard)/dashboard/{{camelCase name}}/page.tsx',
-        templateFile: 'templates/dashboardNav.template.hbs',
+        templateFile: 'templates/dashboard/page.template.hbs',
+      },
+      {
+        type: 'add',
+        path: 'app/(dashboard)/dashboard/{{camelCase name}}/loading.tsx',
+        templateFile: 'templates/dashboard/loading.template.hbs',
+      },
+      {
+        type: 'append',
+        path: 'config/dashboard.ts',
+        pattern: /\/\/append_dashboard_nav/i,
+        templateFile: 'templates/dashboard/config.template.hbs',
       },
     ],
   });
---
+++ client/templates/dashboard/README.md
@@ -0,0 +1,8 @@
+## files affected
+
+command: `npx plop dashboard name`
+
+1. appended `config/dashboard.ts`
+2. new file in `app/_view` folder `NameView.tsx`
+3. Three files in `app/(dashboard)/dashboard` [`name/_components/Name.tsx`,
+   `name/loading.tsx`, `name/page.tsx`]
