929fb7f81a5e5065998ed5eb74e8c3316f7f022b
Akhil Naidu <kaparapu.akhilnaidu@gmail.com>
2024-02-10 02:18:31 +0530

    chore: a different way of getting tokens

--- client/lib/auth.ts
+++ client/lib/auth.ts
@@ -1,11 +1,13 @@
 'use server'
 
-import { cookies } from 'next/headers'
+import { cookies, headers } from 'next/headers'
 
 export async function isAuthenticated() {
   const cookiesList = cookies()
   console.log('cookies', cookiesList)
   console.log('cookies_get_all', cookiesList.getAll())
+  console.log('cookies_using_headers', headers().get('payload-token'))
+  console.log('cookies_using_headers_set_cookie', headers().get('set-cookie'))
   const hasCookie = cookiesList.has('payload-token')
 
   return hasCookie
