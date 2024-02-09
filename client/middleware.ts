55945d08751a6f6cc5329d89dcd64959dd15b368
Akhil Naidu <kaparapu.akhilnaidu@gmail.com>
2024-02-10 01:29:07 +0530

    chore: testing middleware in railway

--- client/middleware.ts
+++ client/middleware.ts
@@ -9,6 +9,8 @@ export const middleware = async (req: NextRequest) => {
   const protectedRoutes = routesConfig.dashboardProtectRoutes
   const pathname = req.nextUrl.pathname
 
+  console.log('middleware triggered', auth)
+
   if (!auth && protectedRoutes.includes(pathname)) {
     const absoluteURL = new URL('/login', req.nextUrl.origin)
     return NextResponse.redirect(absoluteURL.toString())
