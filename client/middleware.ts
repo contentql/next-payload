64695b59fcd3f91d6e2acf7af15d489e741c7f7e
Akhil Naidu <kaparapu.akhilnaidu@gmail.com>
2024-02-10 13:20:20 +0530

    choke: cookie from req

--- client/middleware.ts
+++ client/middleware.ts
@@ -9,9 +9,11 @@ export const middleware = async (req: NextRequest) => {
   const protectedRoutes = routesConfig.dashboardProtectRoutes
   const pathname = req.nextUrl.pathname
 
-  // const cookieToken = req.cookies
-  //   .getAll()
-  //   .find(cookie => cookie.name === 'payload-token')?.value
+  const cookieToken = req.cookies
+    .getAll()
+    .find(cookie => cookie.name === 'payload-token')?.value
+
+  console.log(cookieToken)
 
   if (!auth && protectedRoutes.includes(pathname)) {
     const absoluteURL = new URL('/login', req.nextUrl.origin)
