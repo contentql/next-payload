f621fe4273e174a73dda7f880780f5d3701e20ae
Akhil Naidu <kaparapu.akhilnaidu@gmail.com>
2024-02-10 13:06:56 +0530

    chore: updating client

--- client/lib/auth.ts
+++ client/lib/auth.ts
@@ -1,9 +1,11 @@
 'use server'
 
-import { cookies } from 'next/headers'
+import { cookies, headers } from 'next/headers'
 
 export async function isAuthenticated() {
   const cookiesList = cookies()
+  console.log('cookies', cookiesList)
+  console.log('cookies_get_all', cookiesList.getAll())
 
   const hasCookie = cookiesList.has('payload-token')
 
