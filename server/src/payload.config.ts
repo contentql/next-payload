56b0794875bf3511a8b54430fadd59b944a9db05
julydeath <manojkarajada.mk@gmail.com>
2024-02-14 01:10:20 +0530

    chore : products table added

--- server/src/payload.config.ts
+++ server/src/payload.config.ts
@@ -1,13 +1,14 @@
-import path from 'path';
+import path from "path";
 
-import { payloadCloud } from '@payloadcms/plugin-cloud';
-import { mongooseAdapter } from '@payloadcms/db-mongodb';
-import { webpackBundler } from '@payloadcms/bundler-webpack';
-import { slateEditor } from '@payloadcms/richtext-slate';
-import { buildConfig } from 'payload/config';
+import { payloadCloud } from "@payloadcms/plugin-cloud";
+import { mongooseAdapter } from "@payloadcms/db-mongodb";
+import { webpackBundler } from "@payloadcms/bundler-webpack";
+import { slateEditor } from "@payloadcms/richtext-slate";
+import { buildConfig } from "payload/config";
 
-import Users from './collections/Users';
-import Todos from './collections/Todos';
+import Users from "./collections/Users";
+import Todos from "./collections/Todos";
+import Products from "./collections/Products";
 
 export default buildConfig({
   admin: {
@@ -15,12 +16,12 @@ export default buildConfig({
     bundler: webpackBundler(),
   },
   editor: slateEditor({}),
-  collections: [Users, Todos],
+  collections: [Users, Todos, Products],
   typescript: {
-    outputFile: path.resolve(__dirname, 'payload-types.ts'),
+    outputFile: path.resolve(__dirname, "payload-types.ts"),
   },
   graphQL: {
-    schemaOutputFile: path.resolve(__dirname, 'generated-schema.graphql'),
+    schemaOutputFile: path.resolve(__dirname, "generated-schema.graphql"),
   },
   plugins: [payloadCloud()],
   db: mongooseAdapter({
