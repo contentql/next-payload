14c24977796dfdffeb768b3410f6e1da5c881dca
charanm927 <charanm927@gmail.com>
2024-02-14 10:50:06 +0530

    chore: updated icons and footer

--- client/components/site-footer.tsx
+++ client/components/site-footer.tsx
@@ -1,29 +1,36 @@
-import Link from 'next/link';
-import * as React from 'react';
-
-import { cn } from '@/lib/utils';
-import { ModeToggle } from './mode-toggle';
+import { cn } from '@/lib/utils'
+import Link from 'next/link'
+import * as React from 'react'
+import { AiFillTwitterCircle } from 'react-icons/ai'
+import {
+  IoFlagOutline,
+  IoLogoFacebook,
+  IoLogoGithub,
+  IoLogoYoutube,
+} from 'react-icons/io5'
+import { RiInstagramFill } from 'react-icons/ri'
+import { ModeToggle } from './mode-toggle'
 
 export function SiteFooter({ className }: React.HTMLAttributes<HTMLElement>) {
   return (
     <footer className={cn(className)}>
-      <div className='container mx-auto px-6 lg:px-8 md:pt-12'>
-        <div className='flex flex-col lg:flex-row justify-between space-y-8 lg:space-y-0 lg:space-x-8'>
+      <div className='container mx-auto px-6 md:pt-12 lg:px-8'>
+        <div className='flex flex-col justify-between space-y-8 lg:flex-row lg:space-x-8 lg:space-y-0'>
           <div className='flex flex-col space-y-4'>
-            <FlagIcon className='h-6 w-6 text-[#6366f1]' />
+            <IoFlagOutline size={26} />
             <p>
               Making the world a better place through constructing elegant
               hierarchies.
             </p>
             <div className='flex space-x-4'>
-              <FacebookIcon className='h-6 w-6' />
-              <InstagramIcon className='h-6 w-6' />
-              <TwitterIcon className='h-6 w-6' />
-              <GithubIcon className='h-6 w-6' />
-              <YoutubeIcon className='h-6 w-6' />
+              <IoLogoFacebook size={26} />
+              <RiInstagramFill size={26} />
+              <AiFillTwitterCircle size={26} />
+              <IoLogoGithub size={26} />
+              <IoLogoYoutube size={26} />
             </div>
           </div>
-          <div className='grid grid-cols-2 lg:grid-cols-4 gap-8'>
+          <div className='grid grid-cols-2 gap-8 lg:grid-cols-4'>
             <div className='flex flex-col space-y-2'>
               <h6 className='font-semibold'>Solutions</h6>
               <Link className='block text-gray-400' href='#'>
@@ -86,132 +93,13 @@ export function SiteFooter({ className }: React.HTMLAttributes<HTMLElement>) {
             </div>
           </div>
         </div>
-        <div className='border-t border-gray-700 mt-8 pt-8 flex justify-between pb-6'>
-          <p className='text-gray-400 text-sm text-left'>
+        <div className='flex justify-between pb-6 pt-8'>
+          <p className='text-left text-sm text-gray-400'>
             © 2020 Your Company, Inc. All rights reserved.
           </p>
           <ModeToggle />
         </div>
       </div>
     </footer>
-  );
-}
-
-function FlagIcon(props: any) {
-  return (
-    <svg
-      {...props}
-      xmlns='http://www.w3.org/2000/svg'
-      width='24'
-      height='24'
-      viewBox='0 0 24 24'
-      fill='none'
-      stroke='currentColor'
-      strokeWidth='2'
-      strokeLinecap='round'
-      strokeLinejoin='round'
-    >
-      <path d='M4 15s1-1 4-1 5 2 8 2 4-1 4-1V3s-1 1-4 1-5-2-8-2-4 1-4 1z' />
-      <line x1='4' x2='4' y1='22' y2='15' />
-    </svg>
-  );
-}
-
-function FacebookIcon(props: any) {
-  return (
-    <svg
-      {...props}
-      xmlns='http://www.w3.org/2000/svg'
-      width='24'
-      height='24'
-      viewBox='0 0 24 24'
-      fill='none'
-      stroke='currentColor'
-      strokeWidth='2'
-      strokeLinecap='round'
-      strokeLinejoin='round'
-    >
-      <path d='M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z' />
-    </svg>
-  );
-}
-
-function InstagramIcon(props: any) {
-  return (
-    <svg
-      {...props}
-      xmlns='http://www.w3.org/2000/svg'
-      width='24'
-      height='24'
-      viewBox='0 0 24 24'
-      fill='none'
-      stroke='currentColor'
-      strokeWidth='2'
-      strokeLinecap='round'
-      strokeLinejoin='round'
-    >
-      <rect width='20' height='20' x='2' y='2' rx='5' ry='5' />
-      <path d='M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z' />
-      <line x1='17.5' x2='17.51' y1='6.5' y2='6.5' />
-    </svg>
-  );
-}
-
-function TwitterIcon(props: any) {
-  return (
-    <svg
-      {...props}
-      xmlns='http://www.w3.org/2000/svg'
-      width='24'
-      height='24'
-      viewBox='0 0 24 24'
-      fill='none'
-      stroke='currentColor'
-      strokeWidth='2'
-      strokeLinecap='round'
-      strokeLinejoin='round'
-    >
-      <path d='M22 4s-.7 2.1-2 3.4c1.6 10-9.4 17.3-18 11.6 2.2.1 4.4-.6 6-2C3 15.5.5 9.6 3 5c2.2 2.6 5.6 4.1 9 4-.9-4.2 4-6.6 7-3.8 1.1 0 3-1.2 3-1.2z' />
-    </svg>
-  );
-}
-
-function GithubIcon(props: any) {
-  return (
-    <svg
-      {...props}
-      xmlns='http://www.w3.org/2000/svg'
-      width='24'
-      height='24'
-      viewBox='0 0 24 24'
-      fill='none'
-      stroke='currentColor'
-      strokeWidth='2'
-      strokeLinecap='round'
-      strokeLinejoin='round'
-    >
-      <path d='M15 22v-4a4.8 4.8 0 0 0-1-3.5c3 0 6-2 6-5.5.08-1.25-.27-2.48-1-3.5.28-1.15.28-2.35 0-3.5 0 0-1 0-3 1.5-2.64-.5-5.36-.5-8 0C6 2 5 2 5 2c-.3 1.15-.3 2.35 0 3.5A5.403 5.403 0 0 0 4 9c0 3.5 3 5.5 6 5.5-.39.49-.68 1.05-.85 1.65-.17.6-.22 1.23-.15 1.85v4' />
-      <path d='M9 18c-4.51 2-5-2-7-2' />
-    </svg>
-  );
-}
-
-function YoutubeIcon(props: any) {
-  return (
-    <svg
-      {...props}
-      xmlns='http://www.w3.org/2000/svg'
-      width='24'
-      height='24'
-      viewBox='0 0 24 24'
-      fill='none'
-      stroke='currentColor'
-      strokeWidth='2'
-      strokeLinecap='round'
-      strokeLinejoin='round'
-    >
-      <path d='M2.5 17a24.12 24.12 0 0 1 0-10 2 2 0 0 1 1.4-1.4 49.56 49.56 0 0 1 16.2 0A2 2 0 0 1 21.5 7a24.12 24.12 0 0 1 0 10 2 2 0 0 1-1.4 1.4 49.55 49.55 0 0 1-16.2 0A2 2 0 0 1 2.5 17' />
-      <path d='m10 15 5-3-5-3z' />
-    </svg>
-  );
+  )
 }
