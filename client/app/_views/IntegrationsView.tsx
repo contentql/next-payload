f89f8e6184e1a8be5eb75a32eb94007a5f98bf55
Akhil Naidu <kaparapu.akhilnaidu@gmail.com>
2024-02-15 10:52:55 +0530

    chore: style updates

--- client/app/_views/IntegrationsView.tsx
+++ client/app/_views/IntegrationsView.tsx
@@ -11,8 +11,8 @@ const IntegrationsView = () => {
   return (
     <TabsContent value='integrations'>
       <div className='flex min-h-screen w-full flex-col'>
-        <main className='flex min-h-[calc(100vh-_theme(spacing.16))] flex-1 flex-col gap-4 md:gap-8 md:py-10'>
-          <div className='mx-auto grid w-full max-w-6xl gap-6 md:grid-cols-2 lg:grid-cols-3'>
+        <main className='flex min-h-[calc(100vh-_theme(spacing.16))] flex-1 flex-col gap-4 md:gap-8 md:py-3'>
+          <div className='max-w-8xl mx-auto grid w-full gap-6 md:grid-cols-2 lg:grid-cols-3'>
             {integrations.map(integration => (
               <IntegrationCard key={integration.id} integration={integration} />
             ))}
